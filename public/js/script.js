114033783e25fa014d4cccba8c76b0bb8ad8e59d
Matt <matthewwatkins@mac.com>
2018-11-17 14:47:13 +0100

    - small_screen_nav finished

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 toggleSideNav = function(){
     if($("#small_screen_nav").height() === 0) {
-        $("#small_screen_nav").height(window.innerHeight - $("#navbar").height());
+        $("#small_screen_nav").height("15em");
     } else {
         $("#small_screen_nav").height(0);
     }
