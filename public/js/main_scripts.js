fd5a0092d7584b441087e92c3a22534e2ca917fc
Matt <matthewwatkins@mac.com>
2018-11-22 15:49:01 +0100

    - Progress on content windows

--- public/js/main_scripts.js
+++ public/js/main_scripts.js
@@ -1,10 +1,15 @@
 $(function (){
     $("#burger-menu").click(function(){
         $("#navbar-link-wrapper").toggleClass("visible");
-        $("#dimbg").toggleClass("show");
+        $("#dimbg").toggleClass("visible");
     });
 
     $("#dimbg").click(function(){
         $("#burger-menu").click();
+    });
+
+    $(".navbar-link").click(function(){
+        $("#navbar-link-wrapper").toggleClass("visible");
+        $("#dimbg").toggleClass("visible");
     });
 })
